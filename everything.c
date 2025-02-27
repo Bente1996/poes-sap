79c51bc417cee58880df8bbb81202566d033a51a
Bente1996 <bentedekoning@gmail.com>
2025-02-27 17:22:03 +0100

    selection sort sorted :)

--- everything.c
+++ everything.c
@@ -6,7 +6,7 @@
 /*   By: bde-koni <bde-koni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 17:07:12 by bde-koni          #+#    #+#             */
-/*   Updated: 2025/02/19 15:16:48 by bde-koni         ###   ########.fr       */
+/*   Updated: 2025/02/27 17:10:22 by bde-koni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -493,6 +493,59 @@ int main(int argc, char *argv[]) {
     
     return 0;
 }
+
+// void	four_five(t_node **stack_a, t_node **stack_b)
+// {
+// 	if (stack_a == NULL || *stack_a == NULL) // check nodig?
+// 		return;
+// 	print_list(*stack_a);
+// 	while (stack_size(*stack_a) > 3)
+// 	{
+// 		move_smallest_to_top(stack_a);
+// 		pb(stack_a, stack_b);
+// 	}
+// 	three(stack_a);
+// 	while (*stack_b)
+// 		pa(stack_a, stack_b);
+// }
+
+// void	four_five(t_node **stack_a, t_node **stack_b) // find smallest number, push to b (pb) (look whether closer to top or bottom) until a is empty, pa. 3 numbers: use three()
+// {
+// 	t_node	*lowest;
+// 	t_node	*temp;
+
+// 	if (*stack_a == NULL)
+// 		return;
+
+// 	while (stack_size(stack_a) > 3)
+// 	{
+// 		lowest = *stack_a; // Start search from the beginning
+// 		temp = *stack_a; // make temp so we wont lose head
+
+// 		// Find the node with the smallest value
+// 		while (temp)
+// 		{
+// 			if (temp->value < lowest->value)
+// 				lowest = temp;
+// 			temp = temp->next;
+// 		}
+
+// 		// Move lowest node to top
+// 		while (*stack_a != lowest)
+// 		{
+// 			if (find_smallest(*stack_a) < (stack_size(*stack_a) / 2))
+// 				ra(stack_a); // Rotate up if it's in the first half
+// 			else
+// 				rra(stack_a); // Rotate down if it's in the second half
+// 		}
+
+// 		pb(stack_a, stack_b); // Push lowest node to B
+// 	}
+
+// 	// Push everything back to A
+// 	while (*stack_b)
+// 		pa(stack_a, stack_b);
+// }
 
 // int	main(int argc, char **argv)
 // {
--- little_ones.c
+++ little_ones.c
@@ -6,7 +6,7 @@
 /*   By: bde-koni <bde-koni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/19 16:16:11 by bde-koni          #+#    #+#             */
-/*   Updated: 2025/02/27 17:06:11 by bde-koni         ###   ########.fr       */
+/*   Updated: 2025/02/27 17:19:44 by bde-koni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,57 +44,32 @@ void	three(t_node **stack_a)  // sa ra rra
 	}
 }
 
-// void	four_five(t_node **stack_a, t_node **stack_b)
+// void	three(t_node **stack_a)
 // {
-// 	if (stack_a == NULL || *stack_a == NULL) // check nodig?
-// 		return;
-// 	print_list(*stack_a);
-// 	while (stack_size(*stack_a) > 3)
-// 	{
-// 		move_smallest_to_top(stack_a);
-// 		pb(stack_a, stack_b);
-// 	}
-// 	three(stack_a);
-// 	while (*stack_b)
-// 		pa(stack_a, stack_b);
-// }
-
-// void	four_five(t_node **stack_a, t_node **stack_b) // find smallest number, push to b (pb) (look whether closer to top or bottom) until a is empty, pa. 3 numbers: use three()
-// {
-// 	t_node	*lowest;
-// 	t_node	*temp;
-
+// 	int first;
+// 	int second;
+// 	int third;
 // 	if (*stack_a == NULL)
 // 		return;
-
-// 	while (stack_size(stack_a) > 3)
+// 	first = (*stack_a)->value;
+// 	second = (*stack_a)->next->value;
+// 	third = (*stack_a)->next->next->value;
+// 	if (first > second && second > third) 
 // 	{
-// 		lowest = *stack_a; // Start search from the beginning
-// 		temp = *stack_a; // make temp so we wont lose head
-
-// 		// Find the node with the smallest value
-// 		while (temp)
-// 		{
-// 			if (temp->value < lowest->value)
-// 				lowest = temp;
-// 			temp = temp->next;
-// 		}
-
-// 		// Move lowest node to top
-// 		while (*stack_a != lowest)
-// 		{
-// 			if (find_smallest(*stack_a) < (stack_size(*stack_a) / 2))
-// 				ra(stack_a); // Rotate up if it's in the first half
-// 			else
-// 				rra(stack_a); // Rotate down if it's in the second half
-// 		}
-
-// 		pb(stack_a, stack_b); // Push lowest node to B
+// 		ra(stack_a);
+// 		sa(stack_a);
 // 	}
-
-// 	// Push everything back to A
-// 	while (*stack_b)
-// 		pa(stack_a, stack_b);
+// 	else if (first > second && second < third) 
+// 		ra(stack_a);
+// 	else if (first < second && third < first) 
+// 		rra(stack_a);
+// 	else if (first > second && third > first) 
+// 		sa(stack_a);
+// 	else if (first < third && third < second) 
+// 	{
+// 		rra(stack_a);
+// 		sa(stack_a);
+// 	}
 // }
 
 
